42f234af255e30beffa46108c3b0cbecf4d84a35
Abdul Ahad Shah <111306350+ahadkhaan@users.noreply.github.com>
2024-03-15 11:33:37 +0000

    datatypes

---
+++ 01_basics/datatypes_ecmascript.js
@@ -0,0 +1,21 @@
+"use strict" ; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+// console.log(3+3)                                
+    
+
+//  Primitive DataTypes
+// number => 2 to power 53
+// BigInt 
+// string 
+// boolean=> true/false 
+// null  (stand alone value)
+// undefined 
+// symbol =>use for uniqueness
+
+                     
+console.log(typeof null); //  output will be object
+console.log(typeof undefined); //output will be undefined
+
+
