01f70fe6323b1e56d77840a9107c5a4d7680614e
Abdul Ahad Shah <111306350+ahadkhaan@users.noreply.github.com>
2024-03-15 08:23:40 +0000

    config done

---
+++ 01_basics/const_var_let.js
@@ -0,0 +1,8 @@
+const accountId = 1234
+var name= "Ahad"
+let city = "Peshawar"
+let country; 
+// accountId =2233 not allowed bcz its a const and its doesnt change
+// dont use const bcx its is not good for block scope and fuct scope {}->scope
+
+console.table([accountId, name, city,country])
