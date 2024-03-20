f229faec502137b34f1de797285062f06af8fba9
Abdul Ahad Shah <111306350+ahadkhaan@users.noreply.github.com>
2024-03-20 09:41:44 +0000

    Datatypes Conversion

---
+++ 01_basics/datatypes_conversion.js
@@ -0,0 +1,32 @@
+let score= ""
+
+//console.log(typeof score);
+
+let scoreInNumber = Number (score)
+//console.log(typeof scoreInNumber);
+//console.log(scoreInNumber);
+
+
+// "55" = 55
+// "55abc" = NaN
+// "" = 0
+// true =1 ; false =0
+
+
+// Boolean 
+
+let loggedIn =null
+
+let booleanloggedIn = Boolean (loggedIn)
+//console.log(typeof booleanloggedIn);
+
+// 1 = true 
+// 0 = false
+// null = false
+
+// number to string 
+
+let number = 55
+let stringNumber= String (number)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
