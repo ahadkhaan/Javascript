def2e7a98605a1480cfb0b161fd83c9d763562ea
Abdul Ahad Shah <111306350+ahadkhaan@users.noreply.github.com>
2024-03-23 06:27:53 +0000

    operators

---
+++ 01_basics/operators.js
@@ -0,0 +1,41 @@
+// arthematic operators
+let a = 10
+let b = 4
+
+console.log(a + b) //add
+console.log(a - b) //sub
+console.log(a / b) //divide
+console.log(a * b) //multiply
+console.log(a ** b) //10 will multiply 5 times
+console.log(a % b)// modulus (Show reminder)
+console.log(a++) //first it print the value of a and then it will increment it  
+console.log(a)
+console.log(++a) //in this it will first increament and then show the incremented value of a
+
+// assignment operators
+let c = 4
+
+console.log(c += 1)
+console.log(c -= 1)
+console.log(c *= 1)
+console.log(c /= 1)
+
+console.log(c %= 1)
+
+// comparison operators
+let d = 6
+let e = "6"
+
+console.log(d == e)
+console.log(d != e)
+console.log(d === e)
+console.log(d !== e)
+
+
+// logical operators
+
+let x=7
+let y=6
+console.log(x>y && x==6) // when bth conditions become true it will true
+console.log(x>y || x==5) // when one condition is ture it will be true
+ console.log(!false) //it will make false from true
