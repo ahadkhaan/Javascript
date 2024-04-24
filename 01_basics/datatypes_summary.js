462788d49270d11e40ecc9c262b468a2d383950a
Abdul Ahad Shah <111306350+ahadkhaan@users.noreply.github.com>
2024-04-24 17:19:07 +0000

    datatype summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,12 @@
+// nn ss bb u 
+// null Number string Symbol Boolean BigInt undefined
+
+let num = 33
+let name= "Ahad"
+let bignumb = 223445666883n
+let city;
+let temp=null
+ console.table(typeof[num ,name, bignumb,city,temp])
+
+
+ 
